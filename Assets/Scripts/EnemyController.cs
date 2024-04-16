e50b4b4e75a7e79bf9a28b6c7365f5823a4a7c23
swoofz <jordanhall.1995@yahoo.com>
2024-04-15 18:24:27 -0700

    Enemy Controller
    - Enemies Move
    - Enemies Detect

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace WaveSurvivor {
+    public class EnemyController : MonoBehaviour {
+
+        [SerializeField] private GameObject target;
+        private Unit unit;
+        private Vector3 direction;
+
+        private void Awake() {
+            unit = GetComponent<Unit>();
+            direction = Vector3.zero;
+            target = GameObject.Find("Base");
+            unit.targets.Add(-1, target);
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (!target) return;
+            if (!IsMoving()) unit.GoTo(direction);
+        }
+
+        private bool IsMoving() {
+            if (direction == Vector3.zero) return false;
+
+            direction = target.transform.position;
+            unit.GoTo(direction);
+            return true;
+        }
+
+        public void SwitchTargets(GameObject _target) {
+            target = _target;
+            direction = target.transform.position;
+            unit.GoTo(direction);
+        }
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEngine.GraphicsBuffer;
 
 namespace WaveSurvivor {
+    public enum Faction { Player, Enemy, Neutral }
+
     [RequireComponent(typeof(NavMeshAgent))]
     public abstract class Unit : MonoBehaviour {
+        public Faction faction;
 
         // ENCAPSULATION
         public int Id { get { return id; } set { if (value >= 0) id = value; } }
@@ -16,8 +19,8 @@ namespace WaveSurvivor {
         public float health;
         public float dectionRange;
         public GameObject Indicator { get { return indicator; } private set { indicator = value; } }
+        public Dictionary<int, GameObject> targets = new Dictionary<int, GameObject>();
 
-        private GameObject target;
         private GameObject indicator;
         private SphereCollider dectectionColider;
         private NavMeshAgent nav_Agent;
@@ -27,7 +30,9 @@ namespace WaveSurvivor {
         private void Awake() {
             nav_Agent = GetComponent<NavMeshAgent>();
             dectectionColider = GetComponent<SphereCollider>();
-            indicator = transform.Find("Indicator").gameObject;
+
+            if(gameObject.tag == "Ally")
+                indicator = transform.Find("Indicator").gameObject;
 
             dectectionColider.radius = dectionRange;
         }
@@ -38,7 +43,30 @@ namespace WaveSurvivor {
         }
 
         private void OnTriggerEnter(Collider other) {
-            
+            //Debug.Log(other.gameObject.name);
+
+
+            Unit OtherUnit = other.gameObject.GetComponent<Unit>();
+            if (!OtherUnit) return;
+            if (faction == Faction.Enemy && OtherUnit.faction == Faction.Player) {
+                if (targets.ContainsKey(OtherUnit.Id)) return;
+                targets.Add(OtherUnit.Id, other.gameObject);
+                gameObject.GetComponent<EnemyController>().SwitchTargets(targets.ElementAt(1).Value);
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            //Debug.Log(other.gameObject.name);
+
+
+            Unit OtherUnit = other.gameObject.GetComponent<Unit>();
+            if (!OtherUnit) return;
+            if (faction == Faction.Enemy && OtherUnit.faction == Faction.Player) {
+                targets.Remove(OtherUnit.Id);
+                int index = 0;
+                if (targets.Count > 1) index = 1;
+                gameObject.GetComponent<EnemyController>().SwitchTargets(targets.ElementAt(index).Value);
+            }
         }
 
         public virtual void GoTo(Vector3 _target) {
